17b2137c8e847ba0ebad1140a95e65b96ffdd920
Tenagrim <tenagrim@gmail.com>
2020-09-14 21:53:21 +0500

    cleanup main.c

--- main.c
+++ main.c
@@ -19,7 +19,6 @@ const char	SPACE_S = ' ';
 const int	SCR_W = 60;
 const int	SCR_H = 40;
 const int	START_LINE = 4;
-//const char	ST_FILE[] = "s_t2.txt";
 const char	ST_FILE[] = "states.txt";
 
 typedef struct	{
@@ -173,7 +172,6 @@ void		show_strip(char *s, int size, int pos)
 		end_s++;
 		s_i = 0;
 	}
-	//clrscr();
 	i = 0;
 	gotoxy(1,START_LINE);
 	while(i < end_s -1)
@@ -185,7 +183,6 @@ void		show_strip(char *s, int size, int pos)
 	gotoxy(end_s,START_LINE);
 	while(i < f_i)
 	{
-		//printf("!!!!!!!!!\n");
 		printf("%c", s[i]);
 		i++;
 	}
@@ -204,29 +201,16 @@ void 		control(char *strip, int size, int *pos, char *alph)
 	i = 0;
 	while(c !='q')
 	{
-
 		c = getch();
-		//printf("|%c|\n  >", c);
-		//printf("%d ", i);
-		//ft_putchar(c);
-		//if (c == UP_ARROW)
-			//printf("UP\n");
-		//else if (c == DOWN_ARROW)
-			//printf("DOWN\n");
 		if (c == LEFT_ARROW)
-			 //printf("LEFT\n");
 			*pos = *pos - 1;
 		else if (c == RIGHT_ARROW)
-			//printf("RIGHT\n");
 			*pos = *pos + 1;
 		else if (is_in_alph(alph, c))
 			strip[*pos] = c;
 		else if (c == ' ')
 			strip[*pos] = '_'; 
-		//else 
-			//printf("XZ\n");
 		i++;
-		//printf("(%c)",UP_ARROW);
 		show_strip(strip, size, *pos);
 
 	}
@@ -266,40 +250,28 @@ void		load_states(char *filename, state ****arr, int *size, char **alph)
 	
 	str = malloc(256);
 	*alph = malloc(256);
-	//str = NULL;
 	f = fopen(filename, "r");
 
 	fscanf(f, "%d", size);
 	fscanf(f, "%s", *alph);
 
-	//gotoxy(10,50);
-	//printf("%s" , *alph);
-	//gotoxy(1,1);
-
 	size_w = ft_strlen(*alph);
 
 	init_states(&states, *size, size_w);
-	//printf("++ %d ++\n", *size);
 	i = 0;
 	while (i < *size)
 	{
-		//fscanf(f, "%d\t%d %c %c\t%d %c %c\t%d %c %c\n", &tr, &states[i][0]->q, &states[i][0]->a, &states[i][0]->mv, &states[i][1]->q, &states[i][1]->a, &states[i][1]->mv, &states[i][2]->q, &states[i][2]->a, &states[i][2]->mv );
 		j = 0;
 		fscanf(f, "%d", &tr);
 		while (j < size_w)
 		{
 			fscanf(f, "%d %c %c\t", &states[i][j]->q, &states[i][j]->a, &states[i][j]->mv);
 			j++;
-		}
-		
+		}		
 		i++;
 	}
-	//gotoxy(10,10);
-	//printf("|%d| |%c| |%c|\n", states[0][1]->q, states[0][1]->a, states[0][1]->mv);
-	//gotoxy(1,1);
 	*arr = states;
 	free(str);
-	//*size = 1;
 }
 
 void		print_states(state ***states, int count, char *alph)
@@ -342,8 +314,6 @@ void		mark_state(int cur, int ind, char *m)
 {
 	int x_pos;
 
-
-	//x_pos = 55;
 	gotoxy(ind * 16 + 14, cur + 10);
 	printf("%s",m);
 	gotoxy(1,1);
@@ -388,45 +358,15 @@ void		process(state ***states, char *strip, int size, int *pos, char *alph)
 	state		*c_state;
 	int		c_ind;
 	char		c;
-	//int		delta;
 	int		x_pos;
-	//char		stop;
 
 	c_ind = 0;
 	x_pos = 0;
 	c = '0';
-	//delta = clock();
-	//mark_state(0, "<==");
 
-
-		//gotoxy(70,35);
-
-
-		//x_pos = get_w_ind(alph, strip[*pos]);
-		//printf("%d", x_pos);
-		//fflush(stdout);
-		//sleep(10);
 	do{
-
-		//gotoxy(70,10);
-		//printf("%d  %d", c_ind, x_pos);
 		mark_state(c_ind,x_pos , "  ");
 		x_pos = get_w_ind(alph, strip[*pos]);
-		/*if (strip[*pos] == '_')
-		{
-			c_state = states[c_ind][0];
-			x_pos = 0; 
-		}
-		else if (strip[*pos] == '0')
-		{
-			c_state = states[c_ind][1];
-			x_pos = 1; 
-		}
-		else if (strip[*pos] == '1')
-		{
-			c_state = states[c_ind][2];
-			x_pos = 2; 
-		}*/
 		c_state = states[c_ind][x_pos];
 		strip[*pos] = c_state->a;
 		c_ind = c_state->q - 1;
@@ -436,7 +376,6 @@ void		process(state ***states, char *strip, int size, int *pos, char *alph)
 		else if (c_state->mv == '>')
 			*pos = *pos + 1; 
 		show_strip(strip, size, *pos);
-		//c = getch(); 
 		delay(70000);
 		gotoxy(1,1);
 	}while (c_ind != -2 && c != 'q');
@@ -489,12 +428,6 @@ int		main(int argc, char **argv)
 	init_scr();
 	init_strip(&strip, size);
 	show_strip(strip,size, pos);
-	//load_states(argv[1], &states, &s_count, &alph);
-	//print_states(states, s_count, alph);
-	//gotoxy(60, 60);
-	//printf("%d", ft_strlen("12345"));
-	//gotoxy(1, 1);
-
 	while (1)
 	{
 		load_states(argv[1], &states, &s_count, &alph);
@@ -502,7 +435,6 @@ int		main(int argc, char **argv)
 		control(strip, size, &pos, alph);
 		process(states, strip,size,  &pos, alph);
 		cleanup(states, s_count, strip, alph);
-		//ait_();
 	}
 	cleanup(states, s_count, strip, alph);
 }
